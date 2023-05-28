3a1964196be96a7018a41f717de0d9dbb02dba1b
skoron <astat.popov@yandex.ru>
2023-05-28 16:11:54 +0300

    Теперь создается соединение с обс через websocket

--- ObsPlugin/ObsController.py
+++ ObsPlugin/ObsController.py
@@ -1,20 +1,23 @@
-from asyncio.windows_events import NULL
-import obspython as obs
+import obsws_python as obs
 
 class OBS_API():
-    screen = NULL
+    screen = None
 
     def __init__(self) -> None:
         pass
+
+    def connect(self, host: str, port: int, password: str):
+        client = obs.ReqClient(host=host, port=port, password=password)
+        print(client.create_scene("MyNewScene"))
 
     def getObjOnTheScreen(self):
         pass
 
     def render(self, currentScreen, object):
+        #obs.obs_enter_graphics()
+        #obs.obs_leave_graphics()
         pass
 
-
-#Добавляет описание к скрипту в OBS
 def script_description():
             return """<center><h2>ChatAvtomat v2.0</h2></center>
             <p>Customized chat output from Telegram to OBS (don't forget to reassign)</p>"""
@@ -32,4 +35,4 @@ def script_tick(seconds):
     pass
 
 obsClient = OBS_API()
-script_description()
+obsClient.connect('26.234.129.130', 4455, '8jbzwYaiZtAUFTit')
